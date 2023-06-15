43e8de17718644a0e428d6e77708df96d4b9d059
Amanjeet Singh <amanjeetsingh150@gmail.com>
2023-06-15 14:46:27 +0530

    Extract snapshot in a separate method

--- maestro-ios-xctest-runner/maestro-driver-iosUITests/Routes/Handlers/SubTreeRouteHandler.swift
+++ maestro-ios-xctest-runner/maestro-driver-iosUITests/Routes/Handlers/SubTreeRouteHandler.swift
@@ -4,6 +4,9 @@ import os
 
 @MainActor
 struct SubTreeRouteHandler: HTTPHandler {
+    
+    private static var shouldSwizzleMaxDepth = false
+    
     private let logger = Logger(
         subsystem: Bundle.main.bundleIdentifier!,
         category: String(describing: Self.self)
@@ -14,28 +17,23 @@ struct SubTreeRouteHandler: HTTPHandler {
             logger.error("Requested view hierarchy for an invalid appId")
             return HTTPResponse(statusCode: HTTPStatusCode.badRequest)
         }
+        let xcuiApplication = XCUIApplication(bundleIdentifier: appId)
         
         do {
+            if SubTreeRouteHandler.shouldSwizzleMaxDepth {
+                AccessibilityInterfaceProxy(maxDepth: 2147483647).configureMaxDepth()
+                SubTreeRouteHandler.shouldSwizzleMaxDepth = false
+            }
             let springboardBundleId = "com.apple.springboard"
+            
             logger.info("Trying to capture hierarchy snapshot for \(appId)")
             let start = NSDate().timeIntervalSince1970 * 1000
-            let xcuiApplication = XCUIApplication(bundleIdentifier: appId)
+            
             let springboardApplication = XCUIApplication(bundleIdentifier: springboardBundleId)
             
             SystemPermissionHelper.handleSystemPermissionAlertIfNeeded(springboardApplication: springboardApplication)
             
-            logger.info("[Start] Now trying hierarchy for: \(appId)")
-            var viewHierarchyDictionary = try xcuiApplication.snapshot().dictionaryRepresentation
-            logger.info("[Done] Now trying hierarchy for: \(appId)")
-            logger.info("[Start] Now trying hierarchy for: \(springboardBundleId)")
-            let springboardHierarchyDictionary = try springboardApplication.snapshot().dictionaryRepresentation
-            logger.info("[Done] Now trying hierarchy for: \(springboardBundleId)")
-            
-            let children = viewHierarchyDictionary[XCUIElement.AttributeName(rawValue: "children")] as? Array<[XCUIElement.AttributeName: Any]>
-            let springChildren = springboardHierarchyDictionary[XCUIElement.AttributeName(rawValue: "children")] as? Array<[XCUIElement.AttributeName: Any]>
-            let unifiedChildren = (children ?? [[XCUIElement.AttributeName: Any]]()) + (springChildren ?? [[XCUIElement.AttributeName: Any]]())
-            viewHierarchyDictionary.updateValue(unifiedChildren as Any, forKey: XCUIElement.AttributeName.children)
-            
+            let viewHierarchyDictionary = try getViewHieararchyDictionary(appId: appId, xcuiElement: xcuiApplication)
             
             let end = NSDate().timeIntervalSince1970 * 1000
             logger.info("Successfully got view hierarchy for \(appId) in \(end - start)")
@@ -54,6 +52,27 @@ struct SubTreeRouteHandler: HTTPHandler {
             return HTTPResponse(statusCode: .badRequest, body:  Data(errorJson.utf8))
         }
     }
+    
+    private func getViewHieararchyDictionary(appId: String, xcuiElement: XCUIElement) throws -> [XCUIElement.AttributeName: Any] {
+        let springboardBundleId = "com.apple.springboard"
+        
+        let springboardApplication = XCUIApplication(bundleIdentifier: springboardBundleId)
+        
+        logger.info("[Start] Now trying hierarchy for: \(appId)")
+        var viewHierarchyDictionary = try xcuiElement.snapshot().dictionaryRepresentation
+        logger.info("[Done] Now trying hierarchy for: \(appId)")
+        logger.info("[Start] Now trying hierarchy for: \(springboardBundleId)")
+        let springboardHierarchyDictionary = try springboardApplication.snapshot().dictionaryRepresentation
+        logger.info("[Done] Now trying hierarchy for: \(springboardBundleId)")
+        
+        let children = viewHierarchyDictionary[XCUIElement.AttributeName(rawValue: "children")] as? Array<[XCUIElement.AttributeName: Any]>
+        let springChildren = springboardHierarchyDictionary[XCUIElement.AttributeName(rawValue: "children")] as? Array<[XCUIElement.AttributeName: Any]>
+        let unifiedChildren = (children ?? [[XCUIElement.AttributeName: Any]]()) + (springChildren ?? [[XCUIElement.AttributeName: Any]]())
+        viewHierarchyDictionary.updateValue(unifiedChildren as Any, forKey: XCUIElement.AttributeName.children)
+        
+        return viewHierarchyDictionary
+    }
+    
     
     private func getErrorCode(message: String) -> String {
         if message.contains("Error kAXErrorIllegalArgument getting snapshot for element") {
