5f6d46e7ae91793f273c1d0fe43684fe35abf75f
berik <berik@mobile.dev>
2023-07-05 18:06:26 +0200

    Remove broken test

--- maestro-ios-xctest-runner/XCTestRunner/Tests/XCTestRunnerTests/XCTestRunnerTests.swift
+++ maestro-ios-xctest-runner/XCTestRunner/Tests/XCTestRunnerTests/XCTestRunnerTests.swift
@@ -2,10 +2,7 @@ import XCTest
 @testable import XCTestRunner
 
 final class XCTestRunnerTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(XCTestRunner().text, "Hello, World!")
+    func test() throws {
+        
     }
 }
